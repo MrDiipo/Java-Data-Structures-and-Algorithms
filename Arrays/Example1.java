83194485da76c00b0f734a3857455d1c728ade37
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-07-15 23:41:21 +0100

    Arrays.

---
+++ Arrays/Example1.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import  java.util.Arrays;
+
+public class Example1 {
+    public static void main(String... args) {
+        int[] intArray;
+        intArray = new int[3];
+        intArray[0] = 1;
+        intArray[1] = 2;
+        intArray[2] = 3;
+        System.out.println(Arrays.toString(intArray));
+    }
+}
